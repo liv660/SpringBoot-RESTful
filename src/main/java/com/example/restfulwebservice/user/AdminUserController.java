809cf8ab1541834174ea70530e72a507210498bb
sysim <liv660n@gmail.com>
2021-10-15 14:30:52 +0900

    MIME TYPE을 이용한 REST API Version 관리

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -119,7 +119,7 @@ public class AdminUserController {
     }
     */
 
-    //버전관리 방법 3. Header로 버전 관리 하기
+    /* 버전관리 방법 3. Header로 버전 관리 하기
     @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = userDaoService.findOne(id);
@@ -139,6 +139,49 @@ public class AdminUserController {
     }
 
     @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        //User -> User2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+    */
+
+    //버전관리 방법 4. MIME TYPE으로 버전 관리 하기
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
