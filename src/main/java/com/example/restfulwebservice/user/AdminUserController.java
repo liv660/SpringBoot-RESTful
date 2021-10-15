9b915298d08588146ae77de7071ce63f9ec88ebb
sysim <liv660n@gmail.com>
2021-10-15 14:03:28 +0900

    request parameter를 이용한 REST API Version 관리

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -34,6 +34,7 @@ public class AdminUserController {
         return mapping;
     }
 
+    /* 버전관리 방법 1. URI로 버전 관리 하기
     @GetMapping("/v1/users/{id}")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = userDaoService.findOne(id);
@@ -53,6 +54,47 @@ public class AdminUserController {
     }
 
     @GetMapping("/v2/users/{id}")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        //User -> User2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }*/
+
+    @GetMapping(value = "/users/{id}/", params = "version=1")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping(value = "/users/{id}/", params = "version=2")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
