a9488d940c938d402ed41a4b2c1653b5b6e84559
sysim <liv660n@gmail.com>
2021-10-15 13:28:38 +0900

    @JsonIgnore -> Filter로 변경하여 구현

---
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -0,0 +1,44 @@
+package com.example.restfulwebservice.user;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+
+    private UserDaoService userDaoService;
+
+    //생성자로 의존성 주입
+    public AdminUserController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+}
