3889f0fc9127866de94ad9efe6fa8a18371036e4
sysim <liv660n@gmail.com>
2021-10-18 17:08:40 +0900

    Sprig Data JPA를 이용한 Entity 설정 및 초기 데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user VALUES(1, sysdate(), 'User1', 'test1111', '101010-1111111' );
+INSERT INTO user VALUES(2, sysdate(), 'User2', 'test2222', '202020-2222222' );
+INSERT INTO user VALUES(3, sysdate(), 'User3', 'test3333', '303030-3333333' );
