ab310e362508565a0c76a1621c4d5c69d211a6aa
sysim <liv660n@gmail.com>
2021-10-20 17:47:46 +0900

    게시물 조회를 위한 Post Entity와 User Entity와의 관계 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO user VALUES(90002, sysdate(), 'User2', 'test2222', '202020-2222222' 
 INSERT INTO user VALUES(90003, sysdate(), 'User3', 'test3333', '303030-3333333' );
 
 INSERT INTO post VALUES(10001, 'My First Post', 90001);
-INSERT INTO post VALUES(10002, 'My Second Post', 90002);
+INSERT INTO post VALUES(10002, 'My Second Post', 90001);
